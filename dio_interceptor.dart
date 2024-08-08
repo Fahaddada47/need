9cef02bd6e72dd1a52cd45c77b9a870c8f7833e1
Fahad Islam <fifahad007@gmail.com>
2024-08-08 17:28:11 +0600

    Update dio_interceptor.dart

--- dio_interceptor.dart
+++ dio_interceptor.dart
@@ -2,43 +2,63 @@
 
 import 'dart:convert';
 import 'dart:developer';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio;
 import 'package:get/get.dart';
-import '../controller/auth_controller/login_controller.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import '../model/network_response.dart';
+import '../presentation/auth/auth_controller/login_controller.dart';
+
 
 class NetworkCaller {
-  static final Dio _dio = Dio(BaseOptions(
-    baseUrl: 'https://api.example.com', // Replace with your base URL
-    connectTimeout: 5000,
-    receiveTimeout: 3000,
+  static final dio.Dio _dio = dio.Dio(dio.BaseOptions(
+    baseUrl: 'https://probashiapp.eitrobotics.com/api',
+    connectTimeout: const Duration(milliseconds: 2000),
+    receiveTimeout: const Duration(milliseconds: 2000),
     headers: _defaultHeaders(),
-  ))..interceptors.add(InterceptorsWrapper(
-    onRequest: (options, handler) {
-      log('${options.method} Request: ${options.uri}');
-      log('Request Headers: ${options.headers}');
-      log('Request Data: ${options.data}');
-      return handler.next(options);
-    },
-    onResponse: (response, handler) {
-      _logResponse(response);
-      if (response.statusCode == 401) {
-        _handleUnauthorized();
-      }
-      return handler.next(response);
-    },
-    onError: (DioError e, handler) {
-      log('Error: $e');
-      return handler.next(e);
-    },
-  ));
-
+  ))
+    // ..interceptors.add(dio.InterceptorsWrapper(
+    //   onRequest: (options, handler) {
+    //     log('${options.method} Request: ${options.uri}');
+    //     log('Request Headers: ${options.headers}');
+    //     log('Request Data: ${options.data}');
+    //     return handler.next(options);
+    //   },
+    //   onResponse: (response, handler) {
+    //     _logResponse(response);
+    //     if (response.statusCode == 401) {
+    //       _handleUnauthorized();
+    //     }
+    //     return handler.next(response);
+    //   },
+    //   onError: (dio.DioError e, handler) {
+    //     // log('Error: $e');
+    //     return handler.next(e);
+    //   },
+    // ))
+    ..interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+      maxWidth: 90,
+      filter: (options, args) {
+        if (options.path.contains('/posts')) {
+          return false;
+        }
+        return !args.isResponse || !args.hasUint8ListData;
+      },
+    ));
   /// GET request method
-  static Future<NetworkResponse> getRequest(String path, {Map<String, dynamic>? queryParams}) async {
+  static Future<NetworkResponse> getRequest(String path, {Map<String, dynamic>? queryParams,        bool isLogin = false,
+  }) async {
     try {
-      final response = await _dio.get(
+      final dio.Response response = await _dio.get(
         path,
         queryParameters: queryParams,
+        options: dio.Options(headers: _defaultHeaders(isLogin: isLogin)),
+
       );
 
       if (response.statusCode == 200) {
@@ -68,15 +88,15 @@ class NetworkCaller {
 
   /// POST request method
   static Future<NetworkResponse> postRequest(
-    String path, {
-    Map<String, dynamic>? body,
-    bool isLogin = false,
-  }) async {
+      String path, {
+        Map<String, dynamic>? body,
+        bool isLogin = false,
+      }) async {
     try {
-      final response = await _dio.post(
+      final dio.Response response = await _dio.post(
         path,
         data: jsonEncode(body ?? {}),
-        options: Options(headers: _defaultHeaders(isLogin: isLogin)),
+        options: dio.Options(headers: _defaultHeaders(isLogin: isLogin)),
       );
 
       if (response.statusCode == 200) {
@@ -106,11 +126,11 @@ class NetworkCaller {
 
   /// PUT request method
   static Future<NetworkResponse> putRequest(
-    String path, {
-    Map<String, dynamic>? body,
-  }) async {
+      String path, {
+        Map<String, dynamic>? body,
+      }) async {
     try {
-      final response = await _dio.put(
+      final dio.Response response = await _dio.put(
         path,
         data: jsonEncode(body ?? {}),
       );
@@ -142,11 +162,11 @@ class NetworkCaller {
 
   /// PATCH request method
   static Future<NetworkResponse> patchRequest(
-    String path, {
-    Map<String, dynamic>? body,
-  }) async {
+      String path, {
+        Map<String, dynamic>? body,
+      }) async {
     try {
-      final response = await _dio.patch(
+      final dio.Response response = await _dio.patch(
         path,
         data: jsonEncode(body ?? {}),
       );
@@ -178,11 +198,11 @@ class NetworkCaller {
 
   /// DELETE request method
   static Future<NetworkResponse> deleteRequest(
-    String path, {
-    Map<String, dynamic>? body,
-  }) async {
+      String path, {
+        Map<String, dynamic>? body,
+      }) async {
     try {
-      final response = await _dio.delete(
+      final dio.Response response = await _dio.delete(
         path,
         data: jsonEncode(body ?? {}),
       );
@@ -220,7 +240,7 @@ class NetworkCaller {
     };
   }
 
-  static void _logResponse(Response response) {
+  static void _logResponse(dio.Response response) {
     log('Status Code: ${response.statusCode}');
     log('Response Body: ${response.data}');
   }
